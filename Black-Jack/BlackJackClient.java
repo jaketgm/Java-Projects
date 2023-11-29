a1ba50e01abebee258340c6cc11b6b10a2d8df88
Jake Brockbank <119956222+jaketgm@users.noreply.github.com>
2023-11-29 13:24:14 -0400

    Update BlackJackClient.java

--- Black-Jack/BlackJackClient.java
+++ Black-Jack/BlackJackClient.java
@@ -1,5 +1,21 @@
+/******************************************************************************
+ * @author Jake Brockbank
+ * Nov 29th, 2023 (Original)
+ * Plays a game of black jack.
+******************************************************************************/
+
 import java.util.Scanner;
 
+/******************************************************************************
+ * Method: BlackJackClient (Driver): 
+ * 
+ * - Plays a game of black jack.
+ *
+ * Input: None.
+ *
+ * Output: The game of Black Jack.
+ *
+******************************************************************************/
 public class BlackJackClient 
 {
     public static void main(String[] args) 
@@ -47,6 +63,16 @@ public class BlackJackClient
         scanner.close();
     }
 
+    /******************************************************************************
+     * Method: startNewRound: 
+     * 
+     * - Starts a new round of Black Jack.
+     *
+     * Input: Deck deck, Hand playerHand, Hand dealerHand.
+     *
+     * Output: The new round.
+     *
+    ******************************************************************************/
     private static void startNewRound(Deck deck, Hand playerHand, Hand dealerHand) 
     {
         playerHand.clear();
@@ -58,6 +84,16 @@ public class BlackJackClient
         System.out.println("Round Started.");
     }
 
+    /******************************************************************************
+     * Method: checkForBlackjack: 
+     * 
+     * - Checks to see if anyone has won (i.e. got blackjack (21)).
+     *
+     * Input: Hand playerHand, Hand dealerHand.
+     *
+     * Output: True if blackjack, false if not.
+     *
+    ******************************************************************************/
     private static boolean checkForBlackjack(Hand playerHand, Hand dealerHand) 
     {
         if (playerHand.computeValue() == 21 || dealerHand.computeValue() == 21) 
@@ -68,6 +104,16 @@ public class BlackJackClient
         return false;
     }
 
+    /******************************************************************************
+     * Method: playerTurn: 
+     * 
+     * - Checks to see is its the players turn.
+     *
+     * Input: Scanner scanner, Deck deck, Hand playerHand.
+     *
+     * Output: True so long as no blackjack and not the dealers, false otherwise.
+     *
+    ******************************************************************************/
     private static boolean playerTurn(Scanner scanner, Deck deck, Hand playerHand) 
     {
         while (playerHand.computeValue() < 21) 
@@ -89,6 +135,16 @@ public class BlackJackClient
         return playerHand.computeValue() > 21;
     }
 
+    /******************************************************************************
+     * Method: dealerTurn: 
+     * 
+     * - Checks to see if its the dealers turn.
+     *
+     * Input: Deck deck, Hand dealerHand.
+     *
+     * Output: True so long as no blackjack and not the players, false otherwise.
+     *
+    ******************************************************************************/
     private static boolean dealerTurn(Deck deck, Hand dealerHand) 
     {
         while (dealerHand.computeValue() < 17) 
@@ -98,6 +154,17 @@ public class BlackJackClient
         return dealerHand.computeValue() > 21;
     }
 
+    /******************************************************************************
+     * Method: updateBalance: 
+     * 
+     * - Updates current balance based off when you loose or win a game.
+     *
+     * Input: Hand playerHand, Hand dealerHand, int playerBalance, boolean 
+     * dealerBusted.
+     *
+     * Output: The updated balance.
+     *
+    ******************************************************************************/
     private static int updateBalance(Hand playerHand, Hand dealerHand, int playerBalance, boolean dealerBusted) 
     {
         int playerValue = playerHand.computeValue();
@@ -120,6 +187,16 @@ public class BlackJackClient
         }
     }
 
+    /******************************************************************************
+     * Method: askToPlayAgain: 
+     * 
+     * - Asks player whether they wish to play another game.
+     *
+     * Input: Scanner scanner.
+     *
+     * Output: Closes if no, continues if yes.
+     *
+    ******************************************************************************/
     private static boolean askToPlayAgain(Scanner scanner) 
     {
         System.out.print("Do you want to play again? (yes/no): ");
