1b318c4d5efc31327dc5c103dcbdf11d4226fc0a
Jake Brockbank <119956222+jaketgm@users.noreply.github.com>
2023-11-29 13:26:38 -0400

    Update Card.java

--- Black-Jack/Card.java
+++ Black-Jack/Card.java
@@ -1,24 +1,74 @@
+/******************************************************************************
+ * Class: Card: 
+ * 
+ * - Creates a card with a suit and value.
+ *
+ * Input: None.
+ *
+ * Output: Card with a suit and value.
+ *
+******************************************************************************/
 public class Card 
 {
     private int suitVal;
     private int cardVal;
 
+    /******************************************************************************
+     * Method: Card: 
+     * 
+     * - Initializes a card with a suit and value.
+     *
+     * Input: int suitVal, int cardVal.
+     *
+     * Output: Card with a suit and value.
+     *
+    ******************************************************************************/
     public Card(int suitVal, int cardVal) 
     {
         this.suitVal = suitVal;
         this.cardVal = cardVal;
     }
 
+    /******************************************************************************
+     * Method: getSuit: 
+     * 
+     * - Returns the suite of a card.
+     *
+     * Input: None.
+     *
+     * Output: Suite of card.
+     *
+    ******************************************************************************/
     public int getSuit() 
     {
         return suitVal;
     }
 
+    /******************************************************************************
+     * Method: getValue: 
+     * 
+     * - Returns the value of a card.
+     *
+     * Input: None.
+     *
+     * Output: Value of card.
+     *
+    ******************************************************************************/
     public int getValue() 
     {
         return cardVal;
     }
 
+    /******************************************************************************
+     * Method: toString: 
+     * 
+     * - Prints out the cards suit and value.
+     *
+     * Input: None.
+     *
+     * Output: String with suit and value of card.
+     *
+    ******************************************************************************/
     @Override
     public String toString() 
     {
